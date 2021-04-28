5c36d0b18441959f8cd82d90ea4dced401474f08
Abdulla shafi <abdullashafi5@gmail.com>
2021-04-28 20:21:58 +0600

    first commit

--- myfol/settings.py
+++ myfol/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'folio',
+    'folio.apps.FolioConfig',
 ]
 
 MIDDLEWARE = [
@@ -125,4 +126,5 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'staticfiles')
 ]
 
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+django_heroku.settings(locals())
